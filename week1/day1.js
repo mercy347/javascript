f15ff2a153470babe7fad017b6a3eabed1e58102
mercy347 <MercyMwendeKitheka@gmail.com>
2023-05-31 11:34:13 +0300

    Initial commit

---
+++ week1/day1.js
@@ -0,0 +1,18 @@
+//define the length and width of the rectangle
+let length = 12
+let width =7
+
+//calculate the area
+let area = length * width;
+
+//output the result
+console.log (area)
+
+//define the perimeter of the rectang7le
+ function perimeter(){
+    let length = 10
+    let width = 20
+    console.log(length + width)
+ }
+
+perimeter()
